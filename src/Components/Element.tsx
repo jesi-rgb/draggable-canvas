1b083aa4022ff2ddc7760cc743043ea4193e88c3
Jesús Enrique Rascón <blograso@gmail.com>
2023-02-17 13:22:40 +0100

    damn

---
+++ src/Components/Element.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import Draggable, { DraggableEvent } from "react-draggable";
+
+export default function Element() {
+  const [gridSpacing, setGridSpacing] = useState<number>(1);
+  const handleDrag = (e: DraggableEvent) => {
+    if (e.shiftKey) {
+      setGridSpacing(40);
+      console.log("shit");
+    } else {
+      setGridSpacing(1);
+    }
+  };
+
+  return (
+    <Draggable
+      bounds="parent"
+      grid={[gridSpacing, gridSpacing]}
+      onDrag={(e: DraggableEvent) => handleDrag(e)}
+      defaultPosition={{ x: 10, y: 10 }}
+    >
+      <div className="p-3 border-red-400 bg-red-100 border-2 w-40 h-40 hover:cursor-grab ">
+        <div className="text-center font-bold font-mono">mierda</div>
+        <div className="w-20 h-20 mx-auto mt-5 border-2 bg-blue-100 border-blue-400 "></div>
+      </div>
+    </Draggable>
+  );
+}
